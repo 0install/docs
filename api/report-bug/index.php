dc8577e04dadd8e7d9c4c760868447d2e05ff031
Thomas Leonard <talex5@gmail.com>
2012-08-25 19:34:33 +0100

    Added replacement bug reporting system

---
+++ api/report-bug/index.php
@@ -0,0 +1,39 @@
+<?php
+require("config.php");
+
+$uri = $_POST['uri'];
+$body = $_POST['body'];
+
+if (!isset($uri, $body)) {
+  header("HTTP/1.1 400 Missing fields", true, 400);
+  die("Missing fields in POST");
+  exit(1);
+}
+
+require('Mail.php');
+
+$recipients = "talex5@gmail.com";
+
+$headers = array (
+    'From' => 'bugs@0install.net',
+    'To' => join(', ', $recipients),
+    'Subject' => "Bug in $uri",
+);
+
+$mail_object =& Mail::factory('smtp',
+    array(
+        'host' => 'prwebmail',
+        'auth' => true,
+        'username' => 'zero-install',
+        'password' => $password,
+        //'debug' => true, # uncomment to enable debugging
+    ));
+
+$result = $mail_object->send($recipients, $headers, $body);
+
+if ($result === true) {
+  echo("Bug report email sent");
+} else {
+  header("HTTP/1.1 500 Failed to send email: $result", true, 500);
+  die($result);
+}
