fcdbdfa666a1adc78568bd12332ccd3ddaeaa637
Anders F Bjorklund <afb@users.sourceforge.net>
2012-09-03 23:44:02 +0200

    bump mac pkg to version 1.11

--- detect.php
+++ detect.php
@@ -13,7 +13,7 @@ function get_download_html() {
 		"Ubuntu" => "apt:zeroinstall-injector",
 		"Debian" => "install-linux.html#debian",
 		"FreeBSD" => "install-unix.html#freebsd",
-		"Mac OS X" => "http://downloads.sourceforge.net/project/zero-install/injector/1.7/ZeroInstall.pkg",
+		"Mac OS X" => "http://downloads.sourceforge.net/project/zero-install/injector/1.11/ZeroInstall.pkg",
 		"SUSE" => "install-linux.html#suse",
 		"Fedora" => "install-linux.html#fedora",
 		"Mandriva" => "install-linux.html#mandriva",
