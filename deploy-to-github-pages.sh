e6db78c140740faf45a0143d48f8e5de8322225e
Thomas Leonard <talex5@gmail.com>
2015-06-06 11:04:08 +0100

    GitHub pages doesn't support symlinks

--- deploy-to-github-pages.sh
+++ deploy-to-github-pages.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 if [[ ! -z $(git status -uno --porcelain) ]]; then
   echo Uncommitted changed. Commit them first!
   exit 1
 fi
 
-if [ $(git branch) != "master" ]; then
+if [ $(git rev-parse --abbrev-ref HEAD) != "master" ]; then
   echo "Not on master branch - can't push"
   exit 1
 fi
@@ -13,7 +13,9 @@ rm -rf github-tmp
 git clone . github-tmp
 cd github-tmp
 make
+for f in $(find -type l);do cp --remove-destination $(realpath $f) $f; git add $f; done;
 git add *.html *.php sitemap.txt
 git rm index.xml	# Make sure we don't use this for the index page
+git rm deploy-to-github-pages.sh	# Avoid accidents running it from github-tmp
 git commit -m "Deploy to GitHub Pages"
 git push --force "git@github.com:0install/web-site.git" HEAD:gh-pages
