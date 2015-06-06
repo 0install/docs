4b68f30409920c641df709aaa2990d099011ed54
Thomas Leonard <talex5@gmail.com>
2015-06-06 10:45:32 +0100

    Don't server index.xml as the index page

--- deploy-to-github-pages.sh
+++ deploy-to-github-pages.sh
@@ -14,5 +14,6 @@ git clone . github-tmp
 cd github-tmp
 make
 git add *.html *.php sitemap.txt
+git rm index.xml	# Make sure we don't use this for the index page
 git commit -m "Deploy to GitHub Pages"
 git push --force "git@github.com:0install/web-site.git" HEAD:gh-pages
